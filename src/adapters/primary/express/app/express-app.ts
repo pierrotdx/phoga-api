4a2291d4c9cd5ee758f62bce2434d74ead3ba2bd
pierrotdx <pierrotdx@gmail.com>
2024-08-19 15:06:43 +0200

    use 'helmet' pkg for security

--- src/adapters/primary/express/app/express-app.ts
+++ src/adapters/primary/express/app/express-app.ts
@@ -1,11 +1,13 @@
 import express, { Express } from "express";
 import { AppRouter } from "./routers/app.router";
 import bodyParser from "body-parser";
+import helmet from "helmet";
 
 export class ExpressApp {
   app: Express = express();
 
   constructor(private readonly appRouter: AppRouter) {
+    this.app.use(helmet());
     this.app.use(bodyParser.json());
     this.setRouter();
   }
