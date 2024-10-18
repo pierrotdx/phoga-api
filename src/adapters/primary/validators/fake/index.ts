421ae7647371b428ab005e4856211f8147ddd2d8
pierrotdx <pierrotdx@gmail.com>
2024-10-18 14:37:54 +0200

    add fake search-photo validator

--- src/adapters/primary/validators/fake/index.ts
+++ src/adapters/primary/validators/fake/index.ts
@@ -2,3 +2,4 @@ export * from "./add-photo.fake-validator";
 export * from "./delete-photo.fake-validator";
 export * from "./get-photo.fake-validator";
 export * from "./replace-photo.fake-validator";
+export * from "./search-photo.fake-validator";
