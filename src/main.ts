3e476a7adf353003731b0957e926e6a2e3b25e05
pierrotdx <pierrotdx@gmail.com>
2024-11-02 17:08:29 +0100

    fix small bug

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppLauncher } from "./app-launcher";
 
 const logger = new LoggerWinston();
 const app = new AppLauncher(logger);
-app.start().catch((err) => {
-  logger.error(err);
-});
+app
+  .start()
+  .catch((err) => {
+    logger.error(err);
+  });
