3553074245ac96905b10fb2dbc635d351acf377a
pierrotdx <pierrotdx@gmail.com>
2024-11-21 15:30:14 +0100

    fix glitch

--- src/shared/mongo/core/models/mongo-manager.ts
+++ src/shared/mongo/core/models/mongo-manager.ts
@@ -1,8 +1,10 @@
+import { Collection } from "mongodb";
+
 import { MongoStore } from ".";
 import { MongoCollection } from "./mongo-collection";
 
 export interface IMongoManager {
   open(): Promise<void>;
   close(): Promise<void>;
-  getCollection<T>(collectionName: MongoCollection): MongoStore<T>;
+  getCollection<T>(collectionName: MongoCollection): Collection<MongoStore<T>>;
 }
