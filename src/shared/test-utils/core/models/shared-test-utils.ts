3d51a04a19f672fb9fa8b4529aa1409a715b9ef0
pierrotdx <pierrotdx@gmail.com>
2025-04-19 11:18:05 +0200

    cleaning

--- src/shared/test-utils/core/models/shared-test-utils.ts
+++
@@ -1,9 +0,0 @@
-import { IAssertionsCounter } from "#shared/assertions-counter";
-
-export interface ISharedTestUtils {
-  expectFunctionToBeCalledWith(
-    assertionsCounter: IAssertionsCounter,
-    spy: jest.SpyInstance,
-    ...params: unknown[]
-  ): void;
-}
